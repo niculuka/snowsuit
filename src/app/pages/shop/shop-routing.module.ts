942b75ff8b5d81813973128ff48dcb55d60f2d5c
niculuka <niculuka@yahoo.com>
2025-04-26 23:35:58 +0300

    removed some unused modules

--- src/app/pages/shop/shop-routing.module.ts
+++ src/app/pages/shop/shop-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SidebarPageComponent } from './sidebar/sidebar.component';
-import { NosidebarPageComponent } from './nosidebar/nosidebar.component';
-import { MarketPageComponent } from './market/market.component';
 import { CartComponent } from './cart/cart.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { CheckoutComponent } from './checkout/checkout.component';
@@ -25,19 +23,6 @@ const routes: Routes = [
 		path: '',
 		pathMatch: 'full',
 		redirectTo: 'sidebar/list'
-	},
-	{
-		path: 'nosidebar/:type',
-		component: NosidebarPageComponent
-	},
-	{
-		path: 'nosidebar',
-		pathMatch: 'full',
-		redirectTo: 'nosidebar/boxed'
-	},
-	{
-		path: 'market',
-		component: MarketPageComponent
 	},
 	{
 		path: 'cart',
--- src/app/pages/shop/shop.module.ts
+++ src/app/pages/shop/shop.module.ts
@@ -9,7 +9,6 @@ import { ShopRoutingModule } from './shop-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 
 import { SidebarPageComponent } from './sidebar/sidebar.component';
-import { NosidebarPageComponent } from './nosidebar/nosidebar.component';
 import { CartComponent } from './cart/cart.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { CheckoutComponent } from './checkout/checkout.component';
@@ -18,16 +17,11 @@ import { ProductCategoryBoxedPageComponent } from './product-category-boxed/prod
 import { ProductCategoryFluidPageComponent } from './product-category-fluid/product-category-fluid.component';
 import { ShopSidebarOneComponent } from './shared/sidebar/shop-sidebar-one/shop-sidebar-one.component';
 import { ShopSidebarTwoComponent } from './shared/sidebar/shop-sidebar-two/shop-sidebar-two.component';
-import { ShopSidebarThreeComponent } from './shared/sidebar/shop-sidebar-three/shop-sidebar-three.component';
-import { MarketPageComponent } from './market/market.component';
 import { ShopListOneComponent } from './shared/list/shop-list-one/shop-list-one.component';
-import { ShopListTwoComponent } from './shared/list/shop-list-two/shop-list-two.component';
-import { ShopListThreeComponent } from './shared/list/shop-list-three/shop-list-three.component';
 
 @NgModule( {
 	declarations: [
 		SidebarPageComponent,
-		NosidebarPageComponent,
 		CartComponent,
 		WishlistComponent,
 		CheckoutComponent,
@@ -36,11 +30,7 @@ import { ShopListThreeComponent } from './shared/list/shop-list-three/shop-list-
 		ProductCategoryFluidPageComponent,
 		ShopSidebarOneComponent,
 		ShopSidebarTwoComponent,
-		ShopSidebarThreeComponent,
 		ShopListOneComponent,
-		ShopListTwoComponent,
-		ShopListThreeComponent,
-		MarketPageComponent,
 	],
 	imports: [
 		CommonModule,
